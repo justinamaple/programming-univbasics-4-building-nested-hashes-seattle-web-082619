1a5ac602f3c26c2f0f6875396348b3b239089ec5
Justin Maple <justinamaple@gmail.com>
2019-08-18 22:43:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	    },
 	    :names => {
 	      :reading_railroad => 0,
-	      :a => 0,
+	      :pennsylvania_railroad => 0,
 	      :b_and_o_railroad => 0,
 	      :shortline_railroad => 0,
 	    }
