9106329b73d81a7465394c5f08107bffd3bf2215
Justin Maple <justinamaple@gmail.com>
2019-08-18 22:40:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_pieces_owned => 0,
+	      :two_pieces_owned => 50,
 	      :b => 0,
 	      :c => 0,
 	    },
