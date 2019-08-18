b026e6726e0427344e76a7b577252c85167ca0cd
Justin Maple <justinamaple@gmail.com>
2019-08-18 22:38:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,12 @@ def monopoly_with_third_tier
 	      :b => 0,
 	      :c => 0,
 	    },
-	    :names => []
+	    :names => {
+	      :t => 0,
+	      :a => 0,
+	      :b => 0,
+	      :c => 0,
+	    },
 	  }
   }
 end
