788b5c518b7fff2142032fef9058269b1fe0ebc6
Justin Maple <justinamaple@gmail.com>
2019-08-18 22:03:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
   # keyword "return." Nice and neat.
 	{
 	  :railroads => {
-	    
+	    :pieces => []
 	  }
   }
 end
@@ -13,6 +13,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  	{
+	  :railroads => {
+	    :pieces => []
+	  }
+  }
 end
 
 def monopoly_with_third_tier
